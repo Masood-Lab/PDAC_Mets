d96b3cb083373ed84bab2c5fc5f7338bb42f67c7
ateeq-khaliq <154272206+ateeq-khaliq@users.noreply.github.com>
2026-08-25 14:03:00 -0400

    Update HClustering_Fig2b.R

--- Data/11. Misc/HClustering_Fig2b.R
+++ Data/11. Misc/HClustering_Fig2b.R
@@ -17,7 +17,7 @@ library(cowplot)
  
 config <- config::get()
  
-# source(here::here("pdac_nac", "visualization", "eda.R"))
+# source(here::here("pdac", "visualization", "eda.R"))
 ```
  
 ### Load the seurat object and get the proportions data
@@ -27,18 +27,18 @@ so <- readRDS(here::here(config$data_processed, "06-pdac_CC10_msig.rds"))
  
 # Join with metadata if needed
 metadata <- so@meta.data %>%
-  select(orig.ident, patient_id, neoadjuvant_chemo, CompositionCluster_CC) %>%
+  select(orig.ident, patient,cc_ischia_10) %>%
   rownames_to_column("row_id")
  
  
 # Get the proportions data
-rctd_tier2 <- t(so@assays$rctd_tier2@data)
+rctd_fullfinal <- t(so@assays$rctd_fullfinal@data)
  
 # Ensure the data is in the right format
-rownames(rctd_tier2) <- make.unique(rownames(rctd_tier2))
+rownames(rctd_fullfinal) <- make.unique(rownames(rctd_fullfinal))
  
 # Log transformation of rctd_tier1
-log_comps <- log10(rctd_tier2)
+log_comps <- log10(rctd_fullfinal)
 ```
  
  
@@ -51,12 +51,12 @@ We perform the summary statistics for the RCTD tier data. We perform hierarchica
  
 ```{r}
 # Prepare data for summary statistics
-cluster_summary_pat <- rctd_tier2 %>%
+cluster_summary_pat <- rctd_fullfinal %>%
   as.data.frame() %>%
   rownames_to_column("row_id") %>%
   left_join(metadata, by = "row_id") %>%  # Join with meta_data using row_id as the key
-  pivot_longer(-c(row_id, orig.ident, patient_id, neoadjuvant_chemo, CompositionCluster_CC), values_to = "ct_prop", names_to = "cell_type") %>%
-  group_by(orig.ident, patient_id, neoadjuvant_chemo, CompositionCluster_CC, cell_type) %>%
+  pivot_longer(-c(row_id, orig.ident, patient, cc_ischia_10), values_to = "ct_prop", names_to = "cell_type") %>%
+  group_by(orig.ident, patient, cc_ischia_10, cell_type) %>%
   summarize(median_ct_prop = median(ct_prop, na.rm = TRUE))
 ```
  
@@ -65,13 +65,13 @@ cluster_summary_pat <- rctd_tier2 %>%
 # Aggregate data for median ct prop
 cluster_summary <- cluster_summary_pat %>%
   ungroup() %>%
-  group_by(CompositionCluster_CC, cell_type) %>%
+  group_by(cc_ischia_10, cell_type) %>%
   summarize(patient_median_ct_prop = median(median_ct_prop, na.rm = TRUE))
  
 # Prepare matrix for hierarchical clustering
 cluster_summary_mat <- cluster_summary %>%
   pivot_wider(values_from = patient_median_ct_prop, names_from = cell_type, values_fill = list(patient_median_ct_prop = 0)) %>%
-  column_to_rownames("CompositionCluster_CC") %>%
+  column_to_rownames("cc_ischia_10") %>%
   as.matrix()
  
 # conflicts_prefer(stats::"dist") # resolve conflicts between %*% functions
@@ -88,14 +88,14 @@ ct_order <- hclust(dist(t(cluster_summary_mat)))$labels[hclust(dist(t(cluster_su
  
 # Wilcoxon test for characteristic cell types
 run_wilcox_up <- function(prop_data) {
-  prop_data_group <- prop_data[["CompositionCluster_CC"]] %>% unique() %>% set_names()
+  prop_data_group <- prop_data[["cc_ischia_10"]] %>% unique() %>% set_names()
   map(prop_data_group, function(g) {
     test_data <- prop_data %>%
-      mutate(test_group = ifelse(CompositionCluster_CC == g, "target", "rest")) %>%
+      mutate(test_group = ifelse(cc_ischia_10 == g, "target", "rest")) %>%
       mutate(test_group = factor(test_group, levels = c("target", "rest")))
     wilcox.test(median_ct_prop ~ test_group, data = test_data, alternative = "greater") %>%
       broom::tidy()
-  }) %>% enframe("CompositionCluster_CC") %>% unnest()
+  }) %>% enframe("cc_ischia_10") %>% unnest()
 }
 ```
  
@@ -119,7 +119,7 @@ wilcoxon_res <- cluster_summary_pat %>%
 ```{r}
 # give the path to save the summary statistics
 file_path_cluster_summ <- here::here(config$data_interim,  "summary_of_clusters.txt")
-file_path_wilcox_res <- here::here(config$data_interim, ß "wilcoxon_res_cells_clusters.txt")
+file_path_wilcox_res <- here::here(config$data_interim, "wilcoxon_res_cells_clusters.txt")
  
 # Save the summary statistics
 write.table(cluster_summary_pat, file = file_path_cluster_summ, col.names = TRUE, row.names = FALSE, quote = FALSE, sep = "\t")
@@ -131,22 +131,22 @@ write.table(wilcoxon_res, file = file_path_wilcox_res, col.names = TRUE, row.nam
 ```{r}
 # Plotting mean ct prop and barplots
 mean_ct_prop_plt <- cluster_summary %>%
-  left_join(wilcoxon_res, by = c("CompositionCluster_CC", "cell_type")) %>%
-  mutate(cell_type = factor(cell_type, levels = ct_order), CompositionCluster_CC = factor(CompositionCluster_CC, levels = cluster_order)) %>%
+  left_join(wilcoxon_res, by = c("cc_ischia_10", "cell_type")) %>%
+  mutate(cell_type = factor(cell_type, levels = ct_order), cc_ischia_10 = factor(cc_ischia_10, levels = cluster_order)) %>%
   ungroup() %>%
   group_by(cell_type) %>%
   mutate(scaled_pat_median = (patient_median_ct_prop - mean(patient_median_ct_prop)) / sd(patient_median_ct_prop)) %>%
   ungroup() %>%
-  ggplot(aes(x = cell_type, y = CompositionCluster_CC, fill = scaled_pat_median)) +
+  ggplot(aes(x = cell_type, y = cc_ischia_10, fill = scaled_pat_median)) +
   geom_tile(color = "black") +
   geom_text(aes(label = significant)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 12), legend.position = "bottom", plot.margin = unit(c(0, 0, 0, 0), "cm"), axis.text.y = element_text(size = 12)) +
   scale_fill_gradient2()
  
 cluster_counts <- cluster_info %>%
-  dplyr::select_at(c("row_id", "CompositionCluster_CC")) %>%
-  group_by(CompositionCluster_CC) %>%
-  summarize(nspots = length(CompositionCluster_CC)) %>%
+  dplyr::select_at(c("row_id", "cc_ischia_10")) %>%
+  group_by(cc_ischia_10) %>%
+  summarize(nspots = length(cc_ischia_10)) %>%
   mutate(prop_spots = nspots / sum(nspots))
  
 file_path_cluster_prop_summ <- here::here(config$data_interim, "cluster_prop_summary.csv")
@@ -155,8 +155,8 @@ write_csv(cluster_counts, file_path_cluster_prop_summ)
  
 #barplots for cluster counts
 barplts <- cluster_counts %>%
-  mutate(CompositionCluster_CC = factor(CompositionCluster_CC, levels = cluster_order)) %>%
-  ggplot(aes(y = CompositionCluster_CC, x = prop_spots)) +
+  mutate(cc_ischia_10 = factor(cc_ischia_10, levels = cluster_order)) %>%
+  ggplot(aes(y = cc_ischia_10, x = prop_spots)) +
   geom_bar(stat = "identity") +
   theme_classic() + ylab("") +
   theme(axis.text.y = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"), axis.text.x = element_text(size = 12))
@@ -177,7 +177,7 @@ dev.off()
 pdf_path_boxplot <- here::here(config$plots,  "wilcox_bboxplot_median_ct_prop.pdf")
  
 plt <- cluster_summary_pat %>%
-  ggplot(aes(x = CompositionCluster_CC, y = median_ct_prop)) +
+  ggplot(aes(x = cc_ischia_10, y = median_ct_prop)) +
   geom_boxplot() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   facet_wrap(. ~ cell_type, ncol = 3, scales = "free_y")
